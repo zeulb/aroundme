04b9f79f77c39945f90272ae13440badcf1f6f8b
Stefano Chiesa <base278@gmail.com>
2017-09-14 00:54:43 +0800

    Add react router and material ui deps

--- aroundme/client/src/App.js
+++ aroundme/client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <AppBar
+          title="AroundMe"
+        />
+      </MuiThemeProvider>
     );
   }
 }
