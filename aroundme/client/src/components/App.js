3b30cd348e801cff72077e95ede87322f1d8ca59
Wilson Tan <weeweedry@live.com>
2017-09-23 02:06:35 +0800

    Added FB api call

--- aroundme/client/src/components/App.js
+++ aroundme/client/src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { connect } from "react-redux"
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import Popover from 'material-ui/Popover';
-import Menu from 'material-ui/Menu';
+import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
 import MenuItem from 'material-ui/MenuItem';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import { Page } from '../actions/appActions';
@@ -17,7 +17,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      me: null,
       open: false
     }
   }
@@ -32,12 +31,23 @@ class App extends Component {
       });
       FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
-          this.setState({
-            me: {
-              id: response.authResponse.userID,
-              access_token: response.authResponse.accessToken
+          FB.login(function(response) {
+            if (response.status === 'connected') {
+              FB.api(response.authResponse.userID+"?fields=first_name,last_name,picture.width(58).height(58)", 'get', e=>{
+                this.setState({
+                  id: response.authResponse.userID,
+                  access_token: response.authResponse.accessToken,
+                  first_name: e.first_name,
+                  last_name: e.last_name,
+                  pic_url: e.picture.data.url
+                });
+                localStorage.setItem('name', [e.first_name, e.last_name]);
+                localStorage.setItem('pic_url', e.picture.data.url);
+                localStorage.setItem('me_id', response.authResponse.userID);
+                localStorage.setItem('me_access_token', response.authResponse.accessToken);
+              })
             }
-          })
+          }.bind(this), {scope: 'user_friends'});
         }
       }.bind(this))
     }.bind(this);
@@ -60,11 +70,8 @@ class App extends Component {
   renderBar() {
     return <AppBar
       title="AroundMe"
-      showMenuIconButton={false}
-      iconElementRight={<IconButton 
-                            onTouchTap={this.handleTouchTap.bind(this)}>
-                            <ActionSettings/>
-                          </IconButton>}
+      titleStyle={{textAlign: "center"}}
+      onLeftIconButtonTouchTap={this.handleTouchTap.bind(this)}
     />;
   }
 
@@ -78,21 +85,38 @@ class App extends Component {
     });
   };
 
-  handleFBLogin(e) {
-    console.log(this.state)
+  handleFBLogin(event) {
+    event.preventDefault();
+
     FB.login(function(response) {
       if (response.status === 'connected') {
-        this.setState({
-          me: {
+        FB.api(response.authResponse.userID+"?fields=first_name,last_name,picture.width(58).height(58)", 'get', e=>{
+          this.setState({
             id: response.authResponse.userID,
-            access_token: response.authResponse.accessToken
-          }
+            access_token: response.authResponse.accessToken,
+            first_name: e.first_name,
+            last_name: e.last_name,
+            pic_url: e.picture.data.url
+          });
+          localStorage.setItem('name', [e.first_name, e.last_name]);
+          localStorage.setItem('pic_url', e.picture.data.url);
+          localStorage.setItem('me_id', response.authResponse.userID);
+          localStorage.setItem('me_access_token', response.authResponse.accessToken);
         })
-        FB.api(response.authResponse.userID+"?fields=email,first_name,last_name,name,gender", 'get', e=>{
-          console.log(e);
-          })
       }
     }.bind(this), {scope: 'user_friends'});
+  }
+
+  loadFromCache() {
+    let name = localStorage.getItem('name');
+    name = name? name.split(',') : null;
+    return {
+      id: localStorage.getItem('me_id'),
+      access_token: localStorage.getItem('me_access_token'),
+      first_name: name? name[0] : null,
+      last_name: name? name[1] : null,
+      pic_url: localStorage.getItem('pic_url')
+    };
   }
 
   renderView() {
@@ -104,7 +128,37 @@ class App extends Component {
     }
   }
 
+  handleRequestChange() {
+    this.setState({
+      open: false
+    })
+  }
+
   render() {
+    let cache = this.loadFromCache();
+    let menuTop = null;
+    let menuBottom = null;
+    let menuBottom2 = null;
+    let menuBottom3 = null;
+    console.log(cache);
+    if (this.state.access_token) {
+      menuTop = <div className="Menu-Text">Welcome, {this.state.first_name}!</div>
+      menuBottom = <MenuItem primaryText="My Events" /> 
+      menuBottom2 = <MenuItem primaryText="Settings" />
+      menuBottom3 = <MenuItem primaryText="Sign out" />
+    } else {
+      menuTop = <MenuItem 
+                  style={{"height":"64px",  
+                          "line-height":"64px", 
+                          backgroundColor:"rgb(0,188,212)",
+                          "color": "white",
+                          "font-weight": "bold",
+                          "text-align":"center",
+                          "vertical-align":"middle"}} 
+                  primaryText="Log in"
+                  onClick = {this.handleFBLogin.bind(this)}/>;
+      menuBottom = <MenuItem primaryText="Help &amp; feedback" />;
+    }
     return (
       <div className="App">
         {this.renderBar()}
@@ -114,20 +168,16 @@ class App extends Component {
           visible={this.props.page === Page.MAIN || this.props.page === Page.SELECT_LOCATION}
           selectMode={this.props.page === Page.SELECT_LOCATION}
         />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          onRequestClose={this.handleRequestClose.bind(this)}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-
-          <Menu>
-            <MenuItem primaryText="Refresh" />
-            <MenuItem primaryText="Help &amp; feedback" />
-            <MenuItem primaryText="Settings" />
-            <MenuItem primaryText="Sign out" />
-          </Menu>
-        </Popover>
+        <Drawer 
+          docked={false}
+          onRequestChange={this.handleRequestChange.bind(this)}
+          open={this.state.open}>
+          {menuTop}
+          <Divider />
+          {menuBottom}
+          {menuBottom2}
+          {menuBottom3}
+        </Drawer>
       </div>
     );
   }
@@ -138,3 +188,4 @@ export default connect((store) => {
     page: store.app.page
   };
 })(App);
+
