74026522ea1b25145a4de308c267032439701e72
Stefano Chiesa <base278@gmail.com>
2017-09-22 20:57:31 +0800

    form create event data

--- aroundme/client/src/reducers/formReducer.js
+++ aroundme/client/src/reducers/formReducer.js
@@ -15,7 +15,22 @@ export default function reducer(state={
       return {...state, location: action.payload};
     }
     case "CREATE_EVENT": {
-      console.log(state);
+      var formData = new FormData();
+      // TODO: update this to real user and session id.
+      formData.append('user_id', '1');
+      formData.append('session_id', null);
+      formData.append('long', state.location[0]);
+      formData.append('lat', state.location[1]);
+      formData.append('description', state.description);
+      state.images.forEach(image => {
+        formData.append('content[]', image.file);
+      });
+
+      // TODO: different url for development.
+      fetch("https://api.aroundme.bojio.pw/events", {
+        method: "POST",
+        body: formData
+      });
       return {...state};
     }
     case "RESET": {
