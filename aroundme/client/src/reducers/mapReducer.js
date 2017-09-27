1fb2c99bbd8e2dd04ad947d726570684ced5a36f
Wilson Tan <weeweedry@live.com>
2017-09-28 02:18:06 +0800

    Converts JSON received from BE to geoJSON

--- aroundme/client/src/reducers/mapReducer.js
+++ aroundme/client/src/reducers/mapReducer.js
@@ -11,9 +11,38 @@ export default function reducer(state={
       return {...state, isLoading: false};
     }
     case "FETCH_GEOJSON_FULFILLED": {
-      return {...state, geojson: action.payload, isLoading: false};
+      return {...state, geojson: convert(action.payload), isLoading: false};
     }
     default:
       return state;
+  }
+}
+
+function convert(response) {
+  var geoFeatures = [];
+  for (var i = 0; i < response['events'].length; i++) {
+    var geoEvent = response['events'][i];
+    geoFeatures.push({
+      type: "Feature",
+      properties: {
+        scalerank: 2,
+        name: geoEvent.id,
+        comment: geoEvent.description,
+        name_alt: null,
+        lat_y: geoEvent.lat,
+        long_x: geoEvent.long,
+        region: null,
+        subregion: null,
+        featureclass: null 
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [geoEvent.long, geoEvent.lat]
+      }
+    });
+  }
+  return {
+    type: "FeatureCollection",
+    features: geoFeatures 
   }
 }
