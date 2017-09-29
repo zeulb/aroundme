40165b57ba07bac8428f5ac93e28b38c3dc60a78
Wilson Tan <weeweedry@live.com>
2017-09-29 21:19:06 +0800

    Fixed icon for iphone and help page

--- aroundme/client/src/components/HelpView.js
+++ aroundme/client/src/components/HelpView.js
@@ -7,17 +7,18 @@ class HelpView extends Component {
   render() {
     return (
       <div>
-        <div className="Title">How does "AroundMe" work?</div>
+        <div className="Title">How does Around work?</div>
         <div className="Content"> 
           See something interesting around you? Snap it and upload it on your feed! Or explore whats going on in your area with "Discover Events"! </div>
         <div className="Title">
-          See something interesting and want to share it?
+          I see an interesting event! How do I share it on Around?
         </div>
         <div className="Content">
-          <div className="Content-Inner">1. Select <svg viewBox="0 0 24 24" className="Add-Icon"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg> at the bottom right of your screen in "Discover Events".</div>
-          <div className="Content-Inner">2. Choose <Icons.ImagePhotoCamera/> to snap a shot or choose <Icons.ImagePhotoLibrary /> to upload existing photos.</div>
-          <div className="Content-Inner">3. Drag the pin to where the photo was taken.</div>
-          <div className="Content-Inner">4. Select <Icons.ContentSend /> to upload.</div>
+          <div className="Content-Inner">1. Select "Discover Events" from the menu.</div>
+          <div className="Content-Inner">2. Select <svg viewBox="0 0 24 24" className="Add-Icon"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg> at the bottom left of your screen.</div>
+          <div className="Content-Inner">3. Choose <Icons.ImagePhotoCamera className="Other-Icon"/> to snap a shot or choose <Icons.ImagePhotoLibrary className="Other-Icon"/> to upload existing photos.</div>
+          <div className="Content-Inner">4. Drag the pin to where the photo was taken.</div>
+          <div className="Content-Inner">5. Select <Icons.ContentSend className="Other-Icon"/> to upload.</div>
         </div>
       </div>)
   }
