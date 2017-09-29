7eaa81d5eb297b3f908bc797abfccc39d6418574
Wilson Tan <weeweedry@live.com>
2017-09-29 13:17:33 +0800

    Fix help page (hopefully)

--- aroundme/client/src/components/HelpView.js
+++ aroundme/client/src/components/HelpView.js
@@ -7,16 +7,15 @@ class HelpView extends Component {
   render() {
     return (
       <div>
-        <div className="Title">How it works?</div>
-        <div className="Content"> See an interesting event or news around you? 
-Snap it and upload it on your feed! 
-Looking for an event around your location? 
-Discover events by exploring events that were
-uploaded by other people in your area! </div>
-        <div className="Title">How to upload an event?</div>
+        <div className="Title">How does "AroundMe" work?</div>
+        <div className="Content"> 
+          See something interesting around you? Snap it and upload it on your feed! Or explore whats going on in your area with "Discover Events"! </div>
+        <div className="Title">
+          See something interesting and want to share it?
+        </div>
         <div className="Content">
-          <div className="Content-Inner">1. Select <svg viewBox="0 0 24 24" className="Add-Icon"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>.</div>
-          <div className="Content-Inner">2. Select <Icons.ImagePhotoCamera/> to snap a shot. Select <Icons.ImagePhotoLibrary /> to upload photos.</div>
+          <div className="Content-Inner">1. Select <svg viewBox="0 0 24 24" className="Add-Icon"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg> at the bottom right of your screen in "Discover Events".</div>
+          <div className="Content-Inner">2. Choose <Icons.ImagePhotoCamera/> to snap a shot or choose <Icons.ImagePhotoLibrary /> to upload existing photos.</div>
           <div className="Content-Inner">3. Drag the pin to where the photo was taken.</div>
           <div className="Content-Inner">4. Select <Icons.ContentSend /> to upload.</div>
         </div>
